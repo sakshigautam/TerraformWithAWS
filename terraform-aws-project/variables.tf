84812ef7564b41451d4ebf8da4de757c538bcf2b
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-11-17 18:52:35 +0530

    Update variables.tf

--- terraform-aws-project/variables.tf
+++ terraform-aws-project/variables.tf
@@ -1,1 +1,9 @@
+variable "region" {}
+variable "vpc_cidr" {}
+variable "public_subnets" {}
+variable "azs" {}
+variable "my_ip" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
 
