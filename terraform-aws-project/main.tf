f4c488a314e2d0b0cd7232a8f67d76486bee707b
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-11-17 18:52:18 +0530

    Initialize AWS Terraform configuration with modules

--- terraform-aws-project/main.tf
+++ terraform-aws-project/main.tf
@@ -1,1 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source         = "./modules/vpc"
+  vpc_cidr       = var.vpc_cidr
+  public_subnets = var.public_subnets
+  azs            = var.azs
+}
+
+module "sg" {
+  source            = "./modules/security_groups"
+  vpc_id            = module.vpc.vpc_id
+  allowed_ssh_cidr  = var.my_ip
+  allowed_http_cidr = ["0.0.0.0/0"]
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = module.vpc.public_subnet_ids[0]
+  sg_id         = module.sg.sg_id
+}
