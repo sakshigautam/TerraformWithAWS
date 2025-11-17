f2d86a72e3feb51da40a23b569dfe858aa651de2
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-11-17 18:41:36 +0530

    Update main.tf

--- terraform-aws-project/security_groups/main.tf
+++ terraform-aws-project/security_groups/main.tf
@@ -1,1 +1,27 @@
+resource "aws_security_group" "main" {
+  name        = "allow-ssh-http"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_http_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
